3c4c1eccd8aa35355809512b67b6ba3fc2daf3bf
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-08-11 23:25:19 +0530

    Implimented working accept and read loops

--- main.go
+++ main.go
@@ -2,8 +2,65 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
+type Server struct {
+	listenAddr string
+	listener   net.Listener
+	quitchan   chan struct{}
+}
+
+func NewServer(listenAddr string) *Server {
+	return &Server{
+		listenAddr: listenAddr,
+		quitchan:   make(chan struct{}),
+	}
+}
+
+func (s *Server) start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
+	if err != nil {
+		return err
+	}
+
+	defer ln.Close()
+	s.listener = ln
+
+	go s.acceptLoop()
+
+	<-s.quitchan
+	return nil
+}
+
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			fmt.Println("can not accept connection", err)
+			continue
+		}
+		fmt.Println("accepted new connection", conn.RemoteAddr())
+		go s.readLoop(conn)
+	}
+}
+
+func (s *Server) readLoop(conn net.Conn) {
+
+	buf := make([]byte, 2048)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading messege", err)
+			continue
+		}
+		msg := buf[:n]
+		fmt.Println(string(msg))
+	}
+}
+
 func main() {
-	fmt.Println("hey there")
+	sever := NewServer(":8080")
+	sever.start()
 }
